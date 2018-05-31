64ed5a3c135015d6eebbc5e84de136d118392a40
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-05-31 15:51:06 -0500

    Branching to deploy test to Docker

--- src/middleware/catch-all.js
+++
@@ -1,6 +0,0 @@
-import createError from 'http-errors';
-
-export default (request, response, next) =>
-  request.url === '/' ?
-    response.redirect(process.env.CORS_ORIGIN) :
-    next(createError(404, `__ERROR__ ${request.url} not found`));
